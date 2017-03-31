cc8625a7983327b0907da19b8fb5d70f6c901705
angitha <angitha.das@cabotsolutions.com>
2017-03-31 08:45:00 +0530

    Initial commit

---
+++ app/src/main/java/com/example/angitha/listview/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.angitha.listview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    ListView listView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        listView = (ListView) findViewById(R.id.list);
+
+        String[] values = new String[]{"Android (Alpha)", "Beta", "Cupcake", "Donut", "Eclair",
+                "Froyo", "Gingerbread", "Honeycomb", "Ice Cream Sandwich",
+                "Jelly Bean", "KitKat", "Lollipop", "Marshmallow", "Nougat", "O"
+        };
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, android.R.id.text1, values);
+        listView.setAdapter(adapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                int itemPosition = position;
+                String itemValue = (String) listView.getItemAtPosition(position);
+                Toast.makeText(getApplicationContext(),
+                        "Position :" + itemPosition + "  ListItem : " + itemValue, Toast.LENGTH_LONG)
+                        .show();
+            }
+        });
+    }
+}
